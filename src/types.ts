d5aa63b502963fd8af76591f5319e00ba0b1d987
Sammy Dabbas <dabbassammy@gmail.com>
2026-03-26 00:12:26 -0400

    feat: shared types and constants

---
+++ src/types.ts
@@ -0,0 +1,73 @@
+export type PaperSize = "letter" | "a4" | "legal";
+export type FontName = "times-new-roman" | "arial" | "calibri" | "courier" | "garamond" | "georgia" | "helvetica";
+export type PresetName = "resume" | "letter" | "a4-essay" | "custom";
+
+export interface Margins {
+  top: number;
+  bottom: number;
+  left: number;
+  right: number;
+}
+
+export interface PaperDimensions {
+  width: number;
+  height: number;
+}
+
+export interface PageFormat {
+  paper: PaperDimensions;
+  font: FontName;
+  fontSize: number;
+  margins: Margins;
+  lineSpacing: number;
+}
+
+export interface PageMetrics {
+  charsPerLine: number;
+  linesPerPage: number;
+  charsPerPage: number;
+}
+
+export interface MeasureResultFits {
+  pages: number;
+  target_pages: number;
+  fits: true;
+  characters: number;
+  characters_per_page: number;
+  characters_remaining: number;
+  words: number;
+  percentage_full: number;
+  suggestion: string;
+}
+
+export interface MeasureResultOverflow {
+  pages: number;
+  target_pages: number;
+  fits: false;
+  characters: number;
+  characters_per_page: number;
+  characters_over: number;
+  words: number;
+  percentage_full: number;
+  suggestion: string;
+}
+
+export type MeasureResult = MeasureResultFits | MeasureResultOverflow;
+
+export interface CapacityResult {
+  target_pages: number;
+  max_characters: number;
+  max_words: number;
+  max_lines: number;
+  format_summary: string;
+}
+
+export interface FileResult {
+  file: string;
+  pages: number;
+  format: string;
+}
+
+export const CHARS_PER_WORD = 6;
+export const POINTS_PER_INCH = 72;
+export const MAX_FILE_SIZE = 50 * 1024 * 1024;
